7e806234ac5432057a0d8001b103388087684021
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2022-12-21 00:45:49 +0300

    Proje dosyası ekle.

---
+++ CokBicimlilik/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CokBicimlilik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sekil> sekiller = new List<Sekil>
+            {
+                new Daire(),
+                new Kare(),
+                new Ucgen()
+            };
+            foreach (var sekil in sekiller)
+            {
+                sekil.Ciz();
+            }
+        }
+    }
+    interface Sekil
+    {
+        void Ciz();
+    }
+    class Kare : Sekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("Kare çizildi.");
+        }
+    }
+    class Daire : Sekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("Daire çizildi.");
+        }
+    }
+    class Ucgen : Sekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("Üçgen çizildi.");
+        }
+    }
+}
